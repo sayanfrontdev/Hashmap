9cfae8cd1cae5e83bb17821bf5481c645db4274e
sayanmanda <105246671+sayanmanda@users.noreply.github.com>
2022-07-03 20:53:48 +0530

    Add files via upload

---
+++ find_prime_number.py
@@ -0,0 +1,15 @@
+num=int(input("Enter the number : "))
+prime=True
+for i in range(2,num):
+    if(num%i==0):
+        prime=False
+        break
+if prime:
+     print("This number is prime")
+else:
+    print("This number is not prime")
+
+
+
+
+
